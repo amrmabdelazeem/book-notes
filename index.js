becd78bf1a7f247e846b4ecd44ed1b88dd9d023a
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-12 12:03:08 +0300

    Create index.js main content

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import pg from 'pg';
-import bodyParser from 'body-parser';
-
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.listen((req, res)=>{
-    console.log("Server is running on port "+port+".");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.listen((req, res) => {
+  console.log("Server is running on port " + port + ".");
 });
