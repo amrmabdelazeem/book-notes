16307562da37dea01d351377b5a2e66b4e322bb3
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-12 14:18:31 +0300

    Close DB connection

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get('/', (req, res)=>{
     res.render('index.ejs');
 });
 
+db.end();
+
 app.listen(port, () => {
   console.log("Server is running on port " + port + ".");
   console.log('http://localhost:3000');
