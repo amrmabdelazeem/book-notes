8c59253f083b7fad679caa376f737af184e4c897
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-12 14:09:22 +0300

    Add queries used in DB

---
+++ queries.sql
@@ -0,0 +1,15 @@
+DROP TABLE books, notes IF EXISTS;
+
+CREATE TABLE books(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL,
+	rating INTEGER,
+	review TEXT,
+	date_read DATE NOT NULL
+);
+
+CREATE TABLE notes(
+	note_id SERIAL PRIMARY KEY,
+	book_id INTEGER REFERENCES books(id),
+	note_description TEXT
+);
