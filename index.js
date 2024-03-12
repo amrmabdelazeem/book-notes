5c9a65c37a38564bcab7ee62cfc98e0b3a89d9ff
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-03-12 14:09:01 +0300

    Add dotenv and configure db

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import pg from "pg";
 import bodyParser from "body-parser";
+import { configDotenv } from "dotenv";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
